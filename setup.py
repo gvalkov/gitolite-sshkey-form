e39a68bc29d8bee035a43094b01c7a94f73dd6ce
Georgi Valkov <georgi.t.valkov@gmail.com>
2015-04-14 00:55:33 +0200

    [wip]

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from websshkey import __version__
-from os.path import abspath, dirname, join
+from src import __version__
 from setuptools import setup
 
-
-here = abspath(dirname(__file__))
 
 classifiers = [
     'Programming Language :: Python',
@@ -24,26 +21,29 @@ requirements = [
     'sh>=1.11',
 ]
 
+entry_points = {
+    'console_scripts': [
+        'gitolite_sshkey_form = gitolite_sshkey_form.__main__:main'
+    ]
+}
+
 kw = {
     'name':             'gitolite-sshkey-form',
     'version':          __version__,
-    'description':      'Webapp for collecting and managing public keys',
-    'long_description': open(join(here, 'README.rst')).read(),
+    'description':      'Webapp for collecting and managing ssh public keys',
+    'long_description': open('README.rst').read(),
     'author':           'Georgi Valkov',
     'author_email':     'georgi.t.valkov@gmail.com',
     'license':          'Revised BSD License',
     'url':              'https://github.com/gvalkov/gitolite-sshkey-form',
-    'keywords':         'gitolite sshkey',
-    'packages':         ['websshkey'],
     'classifiers':      classifiers,
+    'keywords':         'gitolite sshkey',
+    'package_dir':      {'gitolite_sshkey_form': 'src'},
+    'packages':         ['gitolite_sshkey_form'],
+    'entry_points':     entry_points,
     'install_requires': requirements,
+    'zip_safe':         False,
     'include_package_data': True,
-    'zip_safe': False,
-    'entry_points': {
-        'console_scripts': [
-            'websshkey = websshkey.__main__:main'
-        ]
-    },
 }
 
 if __name__ == '__main__':
